3168f2f63d448670a201570d9e65355603c2a566
Nicholas Car <nick@kurrawong.ai>
2025-06-12 07:08:59 +1000

    remove web and UI modules

--- vocexcel/web/response.py
+++
@@ -1,8 +0,0 @@
-from fastapi.responses import Response
-
-
-class TurtleResponse(Response):
-    media_type = "text/turtle"
-
-    def render(self, content: str) -> bytes:
-        return content.encode("utf-8")
