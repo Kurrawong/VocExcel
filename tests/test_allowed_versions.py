854b6aca9afdae1423a8b716f0a27c1c8b86e918
Nicholas Car <nick@kurrawong.ai>
2025-06-18 14:41:08 +1000

    improved allow template version tests

--- tests/test_allowed_versions.py
+++ tests/test_allowed_versions.py
@@ -1,12 +1,7 @@
-import json
-import sys
 from pathlib import Path
 
-from rdflib import Literal, URIRef
-from rdflib.namespace import SDO, XSD
 import pytest
 
-from vocexcel.utils import ConversionError
 from vocexcel import convert
 
 TEMPLATES_DIR_PATH = Path(__file__).parent.parent.absolute() / "vocexcel/templates"
@@ -16,3 +11,8 @@ def test_user_specified_versions():
     with pytest.raises(ValueError) as e:
         g = convert.excel_to_rdf(TESTS_DATA_DIR_PATH / "085.xlsx", output_format="graph", allowed_template_versions=["0.8.5.GA"])
         assert "You have restricted the allowed template versions" in e
+
+    try:
+        g = convert.excel_to_rdf(TESTS_DATA_DIR_PATH / "085.xlsx", output_format="graph", allowed_template_versions=["0.8.X"])
+    except ValueError as e:
+        assert "You have restricted the allowed template versions to unknown template versions." in str(e)
